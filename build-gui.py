34ebdc4bb3bd0fb32b586a563d8d1e5df4af7cd7
shinrax2 <shinra.ubu1@yahoo.de>
2025-04-05 12:56:20 +0200

    improve build-gui.py robustness

--- build-gui.py
+++ build-gui.py
@@ -2,7 +2,9 @@ import PyInstaller.__main__
 import os
 import shutil
 
-if os.path.exists("./pdav") and not os.path.exists("./pdav.py"):
+if os.path.exists("./pdav.py") == True:
+    os.remove("./pdav.py")
+if os.path.exists("./pdav") == True and os.path.exists("./pdav.py") == False:
     shutil.copy2("./pdav", "./pdav.py")
 
 PyInstaller.__main__.run([
